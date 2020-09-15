5af74af6381c035c988389b155b0f6d2154dfc47
shinhyogeun <tls1gy2rms3@naver.com>
2020-09-15 17:21:35 +0900

    최후의 50문제(7/50)

--- 5_Project/main.py
+++ 5_Project/main.py
@@ -1,4 +1,4 @@
-# 모험가 길드
+# 1모험가 길드
 
 '''n = int(input())
 arr = list(map(int,input().split()))
@@ -20,7 +20,7 @@ while arr[0]+ss < len(arr):
 
 '''print(count)'''
 
-# 곱하기 혹은 더하기
+# 2곱하기 혹은 더하기
 
 '''arr = list(map(int,input()))
 result = arr[0]
@@ -35,7 +35,7 @@ for i in arr:
 
 print(result)'''
 
-x = 0
+'''x = 0
 y = 1-x
 re = 0
 for i in range(1,10):
@@ -53,5 +53,94 @@ print("세타가 0.5이면 ",((1/9)*(0.5)**5)/0.0167)
 print("세타가 0.6이면 ",((1/9)*(0.6)**3*(0.4)**2)/0.0167)
 print("세타가 0.7이면 ",((1/9)*(0.7)**3*(0.3)**2)/0.0167)
 print("세타가 0.8이면 ",((1/9)*(0.8)**3*(0.2)**2)/0.0167)
-print("세타가 0.9이면 ",((1/9)*(0.9)**3*(0.1)**2)/0.0167)
+print("세타가 0.9이면 ",((1/9)*(0.9)**3*(0.1)**2)/0.0167)'''
+
+# 3문자열 뒤집기
+#이것은 비효율일까 아니면 다른 것일까?
+'''arr = list(map(int,input()))
+zero_zone = 0
+zero = 0
+one_zone = 0
+one = 0
+for i in range(len(arr)):
+    if arr[i] == 0:
+        if one != 0:
+            one_zone += 1
+            one = 0
+        zero += 1
+    elif arr[i] == 1:
+        if zero != 0:
+            zero_zone += 1
+            zero = 0
+        one += 1
+    if i == len(arr)-1 :
+        if arr[i] == 0:
+            zero_zone += 1
+        else:
+            one_zone += 1
+
+print(zero_zone,one_zone)'''
+
+# 다른풀이
+'''arr = list(map(int,input()))
+zero_zone = 0
+one_zone = 0
+for i in range(len(arr)):
+    if i == 0:
+        if arr[0] == 0:
+            zero_zone += 1
+            continue
+        elif arr[i] == 1 :
+            one_zone += 1
+            continue
+    if arr[i] != arr[i-1]:
+        if arr[i] == 1:
+            one_zone += 1
+        elif arr[i] == 0:
+            zero_zone += 1
+print(min(zero_zone,one_zone))'''
+
+# 4.만들 수 없는 금액
+answer = 1
+n = int(input())
+arr = list(map(int,input().split()))
+arr.sort()
+a = 1
+
+def find_remove(a,arr):
+    arr2 = arr[:]
+    if a < 0 or arr2 == []:
+        return False
+    if a == 0:
+        return True
+    for i in range(len(arr2)):
+        if arr2[i] > a:
+            a -= arr2[i-1]
+            del arr2[i-1]
+            return find_remove(a,arr2)
+        if i == len(arr2)-1:
+            a -= arr2[-1]
+            del arr2[-1]
+            return find_remove(a,arr2)
+
+for i in range(1,sum(arr)+1):
+    if find_remove(i,arr) == False:
+        print(i)
+        break
+
+# 5.볼링공 고르기
+
+'''n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+answer = 0
+for i in range(len(arr)-1):
+    for j in range(i+1,len(arr)):
+        if arr[i] != arr[j]:
+          answer += 1
+print(answer)'''
+
+# 무지의 먹방 라이브~
+
+
+
 
