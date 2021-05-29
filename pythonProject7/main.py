69b6eaee8bd732dfcc3d293d8830eae96275711c
shinhyogeun <tls1gy2rms3@naver.com>
2021-05-29 17:41:42 +0900

    베스트 앨범 문제풀이 완료
    link : https://programmers.co.kr/learn/courses/30/lessons/42579

--- pythonProject7/main.py
+++ pythonProject7/main.py
@@ -1,4 +1,21 @@
-//
 
-def solution(asd):
-    for i in
+import heapq
+
+def solution(genres, plays):
+    sortedGenres = {i: [] for i in set(genres)}
+
+    for i in range(len(genres)):
+        heapq.heappush(sortedGenres[genres[i]], (-plays[i], i))
+
+    answers = sorted(sortedGenres.values(), key=lambda x : sum([i[0] for i in x]))
+
+    answer = []
+
+    for ans in answers:
+        for i in range(2):
+            if ans != []:
+                answer.append(heapq.heappop(ans)[1])
+
+    return answer
+
+print(solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500]))
