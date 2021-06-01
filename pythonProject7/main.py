2c4dc7e51a3ac34497dc19043489e736876b95c9
shinhyogeun <tls1gy2rms3@naver.com>
2021-06-01 23:13:43 +0900

    스타 수열 풀이 중

--- pythonProject7/main.py
+++ pythonProject7/main.py
@@ -1,21 +1,44 @@
 
-import heapq
+# import heapq
+#
+# def solution(genres, plays):
+#     sortedGenres = {i: [] for i in set(genres)}
+#
+#     for i in range(len(genres)):
+#         heapq.heappush(sortedGenres[genres[i]], (-plays[i], i))
+#
+#     answers = sorted(sortedGenres.values(), key=lambda x : sum([i[0] for i in x]))
+#
+#     answer = []
+#
+#     for ans in answers:
+#         for i in range(2):
+#             if ans != []:
+#                 answer.append(heapq.heappop(ans)[1])
+#
+#     return answer
+#
+# print(solution(["classic", "pop", "classic", ®"classic", "pop"],[500, 600, 150, 800, 2500]))
 
-def solution(genres, plays):
-    sortedGenres = {i: [] for i in set(genres)}
+def solution(a):
+    if len(a) <= 3:
+        return 0
 
-    for i in range(len(genres)):
-        heapq.heappush(sortedGenres[genres[i]], (-plays[i], i))
+    answerArrs = []
+    answerArrs.append([[a[0]]])
 
-    answers = sorted(sortedGenres.values(), key=lambda x : sum([i[0] for i in x]))
+    target = [[a[0]]]
+    for i in range(len(a)):
+        for j in range(len(answerArrs)):
+            if len(answerArrs[j])//2 == 0:
+                if target[j]:
+            else:
+                answerArr
 
-    answer = []
+        answerArrs.append([a[i]])
+        target.append([a[i]])
 
-    for ans in answers:
-        for i in range(2):
-            if ans != []:
-                answer.append(heapq.heappop(ans)[1])
-
+    answer = -1
     return answer
 
-print(solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500]))
+solution([5,2,3,3,5,3])
