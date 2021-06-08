7f5899c27844dc58fc16346daa7ebca1e7e2e3c0
shinhyogeun <tls1gy2rms3@naver.com>
2021-06-08 23:06:12 +0900

    알고리즘 문제 풀이
    카펫문제
    간단한 문제였던 것 같아요. 약수를 이용해서 구하면 되는거같아요! 조금 더 좋게하자면 sqrt를 이용해서 효율적으로 약수를 구할 수 있겠네요. 다른 분들중에는 조금더 효율적으로 내부블럭의 약수를 이용해서 역으로 푸시는 분들도 있었어요!
    
    뉴스 클러스터링
    이 문제는 카카오 문제였어요! 중복원소가 가능한 집합에서 합집합과 교집합을 구하는 방법이 핵심이었어요! 침착하게 배열로 구하면 쉽게 풀 수 있어요! 다른 분들은 그냥 중복제거하고 중복을 다시 카운트하는 엄청난 생각을 하시는 분들도 있었어요.. ㄷㄷ
    
    소수찾기
    주어진 문자로 만들수 있는 모든 수 중에서 소수가 몇개인지 찾는 문제였어요. 간단한 문제!
    
    최댓값 최솟값 구하기
    너무 쉬워요..
    
    멀쩡한 사각형
    최대공약수에서 반복된다는 것을 찾아내는게 관건이였어요! 대각선은 가로와 세로가 같은 비율로 증가하기에 처음으로 꼭지점에서 만날 때는 y값이 | 넓이/공약수 |. 에서 만나게 되는거에요! 최대 몇 번 반복이 되는지 알려면 최대공약수로 나누면 나오겠죠!?
    
    전화번호목록
    이 문제 재밌었어요! 문자열로 된 숫자를 sort하면 어떻게 되는지 생각해보면 될 것 같네요! 해시를 이용해서 푸는 것도 기억하면 좋을 것 같아요!
    
    행렬의 곱셈
    행렬 그냥 곱셈 구현하면 되는 것 같았어요!
    
    다음큰 숫자
    이건 단순 노가다에요~
    
    올바른 괄호
    케이스를 잘 나눠서 하면 되는 것 같아요! 가벼운 문제! 이런 문제들이 1번으로 잘 나오는 것 같아요!

--- pythonProject7/main.py
+++ pythonProject7/main.py
@@ -372,40 +372,458 @@
 # print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
 # print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
 
-def solution(name):
-    answer = 0
-    target = list(name)
-    now = 0
-
-    for i in range(len(target)):
-        if i == 0:
-            if abs(ord(target[i]) - 65) <= abs(91 - ord(target[i])):
-                answer += abs(ord(target[i]) - 65)
-            else:
-                answer += abs(91 - ord(target[i]))
-            continue
-
-        if target[i] == 'A':
-            continue
-        else:
-            if abs(i-now) <= len(target) - abs(i-now):
-                answer += abs(i-now)
-            else:
-                answer += len(target) - abs(i-now)
-
-            if abs(ord(target[i]) - 65) <= 91 - ord(target[i]):
-                answer += abs(ord(target[i]) - 65)
-            else:
-                answer += abs(91 - ord(target[i]))
-
-            now += abs(i - now)
-
-    return answer
+# def solution(name):
+#     answer = 0
+#     target = list(name)
+#     now = 0
+#
+#     for i in range(len(target)):
+#         if i == 0:
+#             if abs(ord(target[i]) - 65) <= abs(91 - ord(target[i])):
+#                 answer += abs(ord(target[i]) - 65)
+#             else:
+#                 answer += abs(91 - ord(target[i]))
+#             continue
+#
+#         if target[i] == 'A':
+#             continue
+#         else:
+#             if abs(i-now) <= len(target) - abs(i-now):
+#                 answer += abs(i-now)
+#             else:
+#                 answer += len(target) - abs(i-now)
+#
+#             if abs(ord(target[i]) - 65) <= 91 - ord(target[i]):
+#                 answer += abs(ord(target[i]) - 65)
+#             else:
+#                 answer += abs(91 - ord(target[i]))
+#
+#             now += abs(i - now)
+#
+#     return answer
 
 # print(solution("JEROEN"))
 # print(solution("JAZ"))
 # print(solution("ZAZA"))
-print(solution("EEAAE"))
-print(solution("ZZZZZZ"))
-print(solution("BBAAABAAAAAAAAAAAABA"))
+# print(solution("EEAAE"))
+# print(solution("ZZZZZZ"))
+# print(solution("BBAAABAAAAAAAAAAAABA"))
 
+# def solution(priorities, location):
+#     indexArr = [i for i in range(len(priorities))]
+#
+#     nowIndex = -1
+#     answer = 0
+#
+#     while nowIndex != location:
+#         if priorities[0] == max(priorities):
+#             value = priorities.pop(0)
+#             nowIndex = indexArr.pop(0)
+#             answer += 1
+#         else:
+#             value = priorities.pop(0)
+#             now = indexArr.pop(0)
+#             priorities.append(value)
+#             indexArr.append(now)
+#
+#     return answer
+#
+# print(solution([2,1,3,2],2))
+# print(solution([1,1,9,1,1,1],0))
+#
+# print(max([(1,1),(1,2),(1,3)]))
+
+# def solution(s):
+#     s = list(s)
+#     del s[0]
+#     del s[len(s)-1]
+#     s = ''.join(s)
+#
+#     l = []
+#     arr = []
+#
+#     for i in s:
+#         if i == '{':
+#             l = []
+#         elif i == '}':
+#             arr.append(l)
+#         elif i == ',' and :
+#             l.append(int(i))
+#
+#     new = {len(i):i for i in arr}
+#     print(new)
+#     answer = [new[1][0]]
+#
+#     if len(new) >= 2:
+#         for i in range(2, len(arr)+1):
+#             answer.append(list(set(new[i]) - set(new[i-1]))[0])
+#
+#     return answer
+
+# print(solution('{{2},{2,1},{2,1,3},{2,1,3,4}}'))
+# print(solution("{{1,2,3},{2,1},{1,2,4,3},{2}}"))
+# print(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))
+# print(solution("{{20,111},{111}}"))
+
+
+# def solution(s):
+#     answer = ''
+#     aa = s.split(' ')
+#     k = 0
+#     for i in range(len(aa)):
+#         if aa[i] == '':
+#             answer += ' '
+#         else:
+#             k += 1
+#             if 122 >= ord(aa[i][0]) >= 97:
+#                 answer += ''.join([aa[i][k].lower() if k != 0 else aa[i][k].upper() for k in range(len(aa[i]))])
+#             else:
+#                 answer += ''.join([aa[i][k].lower() if k != 0 else aa[i][k] for k in range(len(aa[i]))])
+#
+#             if i != len(aa)-1:
+#                 answer += ' '
+#     if k == 0:
+#         return s
+#     return answer
+#
+# for i in 'asd   ':
+#     print(i,1)
+# print(solution("3people unFollowed me"))
+# print(solution("3people unFolloWed me   a  "))
+# print(solution('   '))
+# print(solution('apple asaa'))
+# print('apple'.split(' '))
+
+# def solution(s):
+#     answer = ''
+#     start = True
+#     for i in s:
+#         if i == ' ':
+#             answer += ' '
+#             start = True
+#         else:
+#             if start:
+#                 start = False
+#                 if 122 >= ord(i) >= 97:
+#                     answer += i.upper()
+#                 else:
+#                     answer += i
+#             else:
+#                 answer += i.lower()
+#
+#     return answer
+#
+# print(solution("3people unFolloWed me   a  "))
+# print(list(' '))
+
+# def solution(numbers):
+#     numbers = sorted((list(map(int,list(str(i)))) for i in numbers),reverse=True)
+#     answer = ""
+#
+#     for i in range(9,-1,-1):
+#         battleArr = [j for j in numbers if j[0] == i]
+#         if battleArr == []:
+#             continue
+#         elif len(battleArr) == 1:
+#             answer += ''.join(map(str,battleArr[0]))
+#         else:
+#
+#
+#
+#
+#     return answer
+# 3 304721 37
+# print(solution([3,30,300,5,9]))
+
+
+
+# yellow = x-2 * y-2
+# brown = 2x+2y-4
+# yellow = x-2 * y-2
+
+# import math
+#
+# def solution(brown, yellow):
+#     total = brown + yellow
+#     answerArr = []
+#     for i in range(1,int(math.floor(math.sqrt(total)))+1):
+#         if int(total/i) == total/i:
+#             answerArr.append(sorted([i,int(total/i)]))
+#
+#     for i in answerArr:
+#         if yellow == (i[0]-2) * (i[1]-2):
+#             return i[::-1]
+#
+# print(solution(10,2))
+
+# def makeSameSet(a,b):
+#     same = []
+#     for i in a:
+#         if i in b:
+#             b.remove(i)
+#             same.append(i)
+#
+#     return same
+#
+# def makeAllSet(a,b):
+#     same = makeSameSet(a[:],b[:])
+#     all = same[:]
+#
+#     while same != []:
+#         real = same.pop(0)
+#
+#         a.remove(real)
+#         b.remove(real)
+#
+#     for k in [a,b]:
+#         for i in k:
+#             all.append(i)
+#
+#     return all
+
+# def solution(str1, str2):
+#     a = [''.join(list(str1)[i:i+2]) for i in range(len(str1)-1)]
+#     b = [''.join(list(str2)[i:i+2]) for i in range(len(str2)-1)]
+#
+#     filtered = [[],[]]
+#     for k in range(2):
+#         for i in [a,b][k]:
+#             if not (90 >= ord(list(i)[0]) >= 65) and not (97 <= ord(list(i)[0]) <= 122):
+#                 continue
+#             if not (90 >= ord(list(i)[1]) >= 65) and not (97 <= ord(list(i)[1]) <= 122):
+#                 continue
+#             filtered[k].append(i.lower())
+#
+#     same = makeSameSet(filtered[0][:],filtered[1][:])
+#     all = makeAllSet(filtered[0][:],filtered[1][:])
+#
+#     if filtered == [[],[]]:
+#         return 65536
+#     return int(65536*(len(same)/len(all)))
+#
+# print(solution('FRANCE','FRENCH'))
+# print(solution('E=M*C^2','e=m*c^2'))
+# print(solution('handshake','shake hands'))
+# print(solution('aa1+aa2', 'AAAA12'))
+
+# import math
+# from itertools import permutations
+#
+# def solution(numbers):
+#     total = []
+#     for i in range(1,len(numbers)+1):
+#         a = list(permutations(numbers,i))
+#         for j in a:
+#             total.append(j)
+#
+#     total = list(set([int(''.join(i)) for i in list(total)]))
+#
+#     if 0 in total:
+#         total.remove(0)
+#     if 1 in total:
+#         total.remove(1)
+#
+#     answer = 0
+#
+#     for i in total:
+#         sosu = True
+#         for j in range(2,int(math.sqrt(i))+1):
+#             if i%j == 0:
+#                 sosu = False
+#                 break
+#         if sosu:
+#             answer += 1
+#
+#
+#     return answer
+#
+# print(solution("011"))
+# print(len(list(permutations("1234567",7))))
+
+# import math
+#
+# def solution(w,h):
+#     multiple = math.gcd(w,h)
+#     answer = 0
+#     if h < w:
+#         a = w
+#         w = h
+#         h = a
+#
+#     for i in range(1, int(w/multiple)+1):
+#         answer += int(math.ceil((h/w)*(i)) - math.floor((h/w)*(i-1)))
+#
+#     return w*h - answer * multiple
+#
+# print(solution(8,12))
+
+# def solution(phone_book):
+#     hash = ' ' + ' '.join(phone_book)
+#
+#     for i in range(len(phone_book)):
+#         a = ' ' + phone_book[i]
+#         if hash.count(a) >= 2:
+#             return False
+#
+#     return True
+
+# def solution(phone_book):
+#     phone_book = sorted(phone_book,key=len)
+#
+#     for i in range(len(phone_book)):
+#         for j in range(i+1,len(phone_book)):
+#             if ' ' + phone_book[i] in ' ' + phone_book[j]:
+#                 return False
+#     return True
+
+# def solution(phone_book):
+#     answer = True
+#
+#     phone_book = sorted(phone_book)
+#     print(phone_book)
+#     for p1, p2 in zip(phone_book, phone_book[1:]):
+#         print(p1,p2)
+#         if p2.find(p1) == 0:
+#             answer = False
+#             break
+#
+#     return answer
+
+# print(solution(["119", "97674223", "1195524421"]))
+# print(solution(["9","10"]))
+
+# def solution(people, limit):
+#     people = sorted(people)
+#     answer = 0
+#
+#     while people != []:
+#         now = 0
+#         now += people.pop(0)
+#
+#         if people == []:
+#             answer += 1
+#             break
+#
+#         while now + people[0] <= limit:
+#             now += people.pop(len(people)-1)
+#             if people == []:
+#                 break
+#
+#         answer += 1
+#
+#     return answer
+# print(solution([70],100))
+# print(solution([70,80,50],100))
+# print(solution([70,80,50,50],100))
+
+import math
+
+
+# def solution(arr):
+#     a = max(arr)
+#     k = 1
+#     while True:
+#         s = 0
+#         for i in arr:
+#             if a*k % i != 0:
+#                 k += 1
+#                 break
+#             else:
+#                 s += 1
+#         if s == len(arr):
+#             break
+#     return a*k
+#
+# print(solution([2,6,8,14]))
+# print(solution([1,1,1,1]))
+
+# def solution(arr1, arr2):
+#     a = len(arr1)
+#     b = len(arr2[0])
+#
+#     answer = [[0 for i in range(b)] for j in range(a)]
+#
+#     for i in range(a):
+#         for j in range(b):
+#             x = arr1[i]
+#             y = [k[j] for k in arr2]
+#             answer[i][j] = sum(zi*zi2 for zi,zi2 in zip(x,y))
+#
+#     return answer
+#
+# print(solution([[1, 4], [3, 2], [4, 1]],[[3, 3], [3, 3]]))
+
+# def binarry(n):
+#     answer = ''
+#     while n > 0:
+#         if n%2 == 1:
+#             answer += '1'
+#         else:
+#             answer += '0'
+#         n //= 2
+#
+#     return ''.join(list(answer)[::-1])
+#
+#
+# def solution(n):
+#     target = binarry(n).count('1')
+#
+#     while True:
+#         n += 1
+#         if binarry(n).count('1') == target:
+#             break
+#
+#     return n
+#
+# print(solution(1502051))
+
+# def solution(s):
+#     arr = list(s)
+#
+#     plate = []
+#
+#     for i in arr:
+#         if i == '(':
+#             plate.append('(')
+#         else:
+#             if plate == []:
+#                 return False
+#             else:
+#                 if plate[-1] == '(':
+#                     del plate[-1]
+#
+#     if plate == []:
+#         return True
+#     return False
+#
+# print(solution(')()('))
+
+def isSquareExist(board,number):
+    a = len(board) - number
+    b = len(board[0]) - number
+
+    for i in range(a+1):
+        for j in range(b+1):
+            trigger = False
+            for k in range(number):
+                for u in range(number):
+                    if board[i+k][j+u] == 0:
+                        trigger = True
+                        break
+                if trigger:
+                    break
+            if trigger:
+                continue
+            return True
+    return False
+
+def solution(board):
+    short = len(board) if len(board[0]) >= len(board) else len(board[0])
+    for i in range(short,0,-1):
+        if isSquareExist(board,i):
+            return i**2
+    return 0
+
+print('답: ',solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
+print('답: ',solution([[0,0,1,1],[1,1,1,1]]))
+print('답: ',solution([[0,0,0,0]]))
